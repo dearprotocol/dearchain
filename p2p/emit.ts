4ab0e109454f3c9a09aea7c140b9a402f2e0329f
Sanchita6365 <sanchita@codepartner.in>
2022-09-09 15:48:45 +0530

    new

--- p2p/emit.ts
+++ p2p/emit.ts
@@ -1,4 +1,16 @@
-// Hosts a Server
+import {connections} from "../packages/db/memory/wss";
+
+// Hosts a Serv
+async function send(data:string){
+    console.log("connections",connections);
+    for(let connection of connections){
+        connection.sendUTF(data)
+    }
+}
+setInterval(() =>{
+     send("hi");
+}, 5000)
+
 
 // BLOCK ADDED
 
