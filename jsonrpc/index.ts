a909a9b1c0293e002db945eaef66caa8d7c86528
Sanchita6365 <sanchita@codepartner.in>
2022-09-12 18:24:46 +0530

    getbalance

--- jsonrpc/index.ts
+++ jsonrpc/index.ts
@@ -2,17 +2,30 @@ import express from 'express'
 import https from 'https'
 import * as fs from 'fs';
 import * as path from 'path';
+import { getBalance } from './modules/address/getBalance';
+import { getNonce } from './modules/address/getNonce';
 const app = express();
+interface iMethods {
+    [key:string]:any
+}
+const methods:iMethods={
+    "getbalance":getBalance,
+    "getnonce":getNonce,
+}
 
+app.use(express.json())
 app.use('/',(req,res,next)=>{
 
     if(req.method!="POST"){
         console.log(req.ip);
         res.send("Only POST request is allowed in JSON RPC server")
     }else{
-        console.log(req.body);
-        console.log(req.ip);
-        res.send("Good to See You")
+        // console.log("body",req.body);
+        // console.log(req.ip);
+        let method:string =req.body.method;
+        res.send({
+            jsonrpc:req.body.jsonrpc,result:methods[method](req.body.params)
+        })
     }
 });
 let server = https.createServer({
--- jsonrpc/modules/address/getNonce.ts
+++ jsonrpc/modules/address/getNonce.ts
@@ -1,1 +1,6 @@
 //  MAKE IT
+import {AddressDB} from "../../../packages/db/memory/address"
+export const getNonce =(address:string[]) =>{
+let index:string = address[0];
+return AddressDB[index].nonce;
+}
