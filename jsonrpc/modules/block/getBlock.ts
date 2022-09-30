67144cc3a964db77443cbf056b8d86eee20b19fe
Mukesh Roy <mkshr8@gmail.com>
2022-09-30 17:54:45 +0530

    getBlock || getTransaction

--- jsonrpc/modules/block/getBlock.ts
+++ jsonrpc/modules/block/getBlock.ts
@@ -1,1 +1,33 @@
-//  MAKE IT
+
+import { Trie, LevelDB } from '@ethereumjs/trie'
+import { Level } from 'level'
+const blockDB =new  Level('../../../DEARCHAIN_BLOCK_DB')
+
+
+export function getBlock(){
+    blockDB.open
+let arr:any = []; 
+    let iterator:any = blockDB.iterator().all(function (err, data) {
+        if(err){
+
+        }
+        else{
+            console.log(data)
+        }
+    })
+
+    arr.push(iterator)
+
+
+    console.log(arr)
+
+//   return keys
+
+    return arr
+}
+
+
+
+
+
+getBlock();
